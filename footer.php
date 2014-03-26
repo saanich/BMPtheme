c5a23014d5511eca1d055ed7035408ac16246a52
saanich <joel.friesen@saanich.ca>
2014-03-26 08:41:24 -0700

    1.6 Added footer text, changed BG in IE8

--- footer.php
+++ footer.php
@@ -1,12 +1,10 @@
     </div>
 
 
-<div class="footer main">
-    <div class="container">
-    	<div class="foottext">
-        	<?php dynamic_sidebar('footer') ?>
-    	</div>
-    </div>
+<div class="footer main container">
+	<div class="foottext">
+		<p>District of Saanich - 770 Vernon Ave Victoria, BC, Canada V8X 2W7 Tel: 250-475-1775 (8:30am - 4:30pm)</p>
+	</div>
 </div>
 <?php wp_footer(); ?>
 </body>
